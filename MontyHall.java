2bc69bb4df4700b177c201de7ecd8c4f29d061cf
Lindsey Yu ッ <lyu2018@pingry.org>
2016-10-02 23:39:21 -0400

    Commented with javadocs.

--- MontyHall.java
+++ MontyHall.java
@@ -1,12 +1,28 @@
 
 import java.util.Scanner;
 
-public class MontyHall {
 
-    //Declaring things here, makes them fields. They are accesible everywhere in the class.
-    public static Scanner kb = new Scanner(System.in); 
+ 
+public class MontyHall {
+    
+
+    /**
+ * This class represents a program that 
+ * simulates the Monty Hall Problem.
+ * @author Lindsey Yu
+ * @version 1
+ */
+
+
+
+     /**
+      * Main method calls the getUserDoor, revealDoor, getUserSwap, and newDoor methods
+      * @param args Standard array of commandline String arguments. Not used in this program.
+      */    
 
     public static void main(String[] args) {
+
+            
 
         //model the doors as ints
 
@@ -22,14 +38,14 @@ public class MontyHall {
         boolean swap = getUserSwap();
 
         
-        if (swap == true) {
+        if (swap == true) { 
             userDoor = newDoor(userDoor, goatDoor);
-            System.out.println("Your new door number is " + userDoor); }
+            System.out.println("Okay, your new door number is " + userDoor + "."); }
         else {
             userDoor= userDoor;
-            System.out.println("You decided to stay. Your door number is still " + userDoor); }
+            System.out.println("You decided to stay. Your door number is still " + userDoor + "."); }
 
-        String object = " ";
+        String object = " "; //create object string
 
 
         if (userDoor == carDoor) 
@@ -38,7 +54,7 @@ public class MontyHall {
              object = "goat";
 
         
-        if (object.equals("goat")) {
+        if (object.equals("goat")) { //ASCII art!
             System.out.println("Sorry, behind Door #" + userDoor + " is a " + object); 
             System.out.println("(_( \n/_/'_____/)\n\"  l      l\n   l\"\"\"\"\"\"l");}
         else { 
@@ -46,12 +62,14 @@ public class MontyHall {
             System.out.println("  ______\n ll_ll_\\`.__\n(   _    _ _\\\n=`-(_)--(_)-'");}
         }
 
-      
+ 
 
-    /** Method as the user whether they want to swap their or not.
+    public static Scanner kb = new Scanner(System.in); 
+
+
+    /** This method asks the user whether they want to swap their or not.
     *
-    * @return Returns a boolean whether the user wants to swap. (true to swap, false to stay)
-
+    * @return Returns a boolean whether the user wants to swap or not. (true to swap, false to stay)
     */
 
     public static boolean getUserSwap() {
@@ -71,7 +89,7 @@ public class MontyHall {
     * This method calcs and teturns a door for the host to reveals as not the GP door
     * @param invalid1 A door that the host cannot open
     * @param invalid2 A door ths the host cannot open
-    * @returm number of a door containing a goat
+    * @return number of a door containing a goat
     */
     public static int revealDoor(int invalid1, int invalid2) {
         
@@ -85,14 +103,27 @@ public class MontyHall {
         //javac needs to know that *something* is always returned
 
         return -1; }
+    
 
+    /**
+    * This method asks the user to choose a door number
+    * @return number of a door picked by user
+    */
      public static int getUserDoor() {
     
         System.out.println("Choose a door between 1-3: ");
         String input = kb.nextLine();
         return Integer.parseInt(input);  }
 
+
     
+    /**
+    * This method switches the userDoor to the only other open door (not the goat door or the user door)
+    * @param userDoor the original door the user picked
+    * @param goatDoor the door the program revealed that contains a goat
+    * @return the number of the new user door after they switched
+    */
+
     public static int newDoor(int userDoor, int goatDoor) {
         
         for (int newDoor =1; newDoor <4; newDoor++) {
